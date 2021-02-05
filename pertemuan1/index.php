591636275bfc4397ce20de48919134764fb46a0a
Chairunissa <chairunissap37@gmail.com>
2021-02-05 15:36:56 +0700

    membuat hello world

---
+++ pertemuan1/index.php
@@ -0,0 +1,12 @@
+<?php
+/*
+    Chairunissa Putri Koswara
+    203040048
+    github.com/Chairunissa/pw2021_203040048.git
+    Pertemuan 1 (5 Februari 2021)
+    membuat hello world
+*/
+?>
+<?php
+    echo "Hello World!";
+?>
